9d2b60c97120e15b437e9eb34f259a78b2ca5db5
Eliud254 <eliudobure@gmail.com>
2023-10-05 07:09:04 -0700

    python project

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    a = 1
-    b = 2
-    from add_0 import add
+if _name_ == "_main_"
 
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+"""print sum of 1 and 2"""
+
+from add_0 import add
+
+a = 1
+b = 2
+
+print("{} + {} = {}" .format(a, b, add(a, b)))
