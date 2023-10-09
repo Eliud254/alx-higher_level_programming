9604867da6335215faf46f41f63246dbb2b113cc
Eliud254 <eliudobure@gmail.com>
2023-10-09 08:51:37 -0700

    pythone project

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+
+
+def replace_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
