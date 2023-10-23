4ffbd1c5a1c2205085052f269d9ede157aae0c6d
Eliud254 <eliudobure@gmail.com>
2023-10-23 15:44:48 -0700

    python project

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,9 +17,9 @@ def list_division(my_list_1, my_list_2, list_length):
                 result.append(0)
         except ZeroDivisionError:
             print("division by 0")
-            result.append(0.0)
+            result.append(0)
         except IndexError:
             print("out of range")
-            result.append(0.0)
+            result.append(0)
 
     return result
