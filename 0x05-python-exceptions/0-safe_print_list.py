1e170f59ba69ddec1f9f1fca38410bf177f539a8
Eliud254 <eliudobure@gmail.com>
2023-10-23 14:21:33 -0700

    python project

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
-
+    count = 0
     for i in range(x):
         try:
-            if i >= len(my_list):
-                break  # Stop if we've reached the end of the list
-            print(my_list[i], end="")
-            num += 1
+            print("{}".format(my_list[i]), end='')
+            count += 1
         except IndexError:
             break
-
     print()
-    return num
+    return (count)
