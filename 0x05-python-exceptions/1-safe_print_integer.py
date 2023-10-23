98853f8510e62a4f5d985c9a5cb8b30b9af67887
Eliud254 <eliudobure@gmail.com>
2023-10-23 14:57:30 -0700

    python project

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
-#!/usr/bin/pyhon3
+#!/usr/bin/python3
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except ValueError:
         return False
