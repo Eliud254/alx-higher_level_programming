246164f78eb32444e39d8dad4ebf822a340d1b86
Eliud254 <eliudobure@gmail.com>
2023-10-24 14:15:53 -0700

    python project

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """The singly linked list"""
 
-    class Node:
-        """The class which is defining the node"""
-    def __init__(self, data, next_node=None):
-        if not isinstance(data, int):
-            raise TypeError("data must be an integer")
-        if next_node is not None and not isinstance(next_node, Node):
-            raise TypeError("next_node must be a Node object")
+class Node:
+    """ The class which  defines the node"""
+    def __init__(self, data, next_node):
         self.__data = data
         self.__next_node = next_node
-
     @property
     def data(self):
         return self.__data
@@ -18,7 +13,7 @@
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
-            raise TypeError("data must be an integer")
+            raise TypeError('data must be an int')
         self.__data = value
 
     @property
@@ -27,31 +22,9 @@
 
     @next_node.setter
     def next_node(self, value):
-        if value is not None and not isinstance(value, Node):
-            raise TypeError("next_node must be a Node object")
-
+        if not isinstance(value, Node) and vaue != None:
+            raise TypeError('value must be a node object')
 
 class SinglyLinkedList:
     def __init__(self):
         self.__head = None
-
-    def sorted_insert(self, value):
-        new_node = Node(value)
-        if self.__head is None or value < self.__head.data:
-            new_node.next_node = self.__head
-            self.__head = new_node
-        else:
-            current = self.__head
-            while current.next_node is not None and current.next_node.data < value:
-                current = current.next_node
-            new_node.next_node = current.next_node
-            current.next_node = new_node
-
-    def __str__(self):
-        result = ""
-        current = self.__head
-        while current:
-            result += str(current.data) + "\n"
-            current = current.next_node
-        return result.strip()
-
