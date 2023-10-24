2c980705cd31608de9c26f361c8954b5ae12c49a
Eliud254 <eliudobure@gmail.com>
2023-10-24 13:27:03 -0700

    python project

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Define the square and the size"""
+
 
 class Square:
+    """Solving the square"""
+
     def __init__(self, size=0):
+        """initialize the new square"""
         self.__size = size
