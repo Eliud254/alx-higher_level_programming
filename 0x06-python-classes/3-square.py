bc1729e3537a9f00f3666a1ab5df77cf4683147d
Eliud254 <eliudobure@gmail.com>
2023-10-24 13:45:49 -0700

    python project

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""define the square as per 2-square.py"""
 
 
 class Square:
+    """defining the square by size"""
     def __init__(self, size=0):
         self.__size = size
         if not isinstance(size, int):
@@ -11,5 +13,6 @@ class Square:
             raise ValueError('size must be >= 0')
 
     def area(self):
+        """returns the area"""
         area = self.__size * self.__size
         return area
