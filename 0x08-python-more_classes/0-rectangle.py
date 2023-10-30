b17ab6930d8996ce3e96c7438be69d69a3659f3d
Eliud254 <eliudobure@gmail.com>
2023-10-30 02:38:48 -0700

    python project

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-# class Rectangle that defines a rectangle
+""" class Rectangle that defines a rectangle"""
+
+
 class Rectangle:
+    """A class which is empty defining rectangle"""
     pass
