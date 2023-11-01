8a493ff3a07886dd3cdf738b1e8fddbf6a8d295c
Eliud254 <eliudobure@gmail.com>
2023-11-01 13:15:25 -0700

    python project

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
-def magic_string(word_list=[]):
-    word_list += ["Holberton"]
-    return ", ".join(word_list)
+def magic_string():
+    magic_string.word_list = getattr(magic_string, 'word_list', []) + ["Holberton"]
+    return ", ".join(magic_string.word_list)
+
+if __name__ == "__main__":
+    for i in range(10):
+        print(magic_string())
