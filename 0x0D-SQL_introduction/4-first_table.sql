5bec51f20866951c7fdd24a9db7eaece6bc1726a
Eliud254 <eliudobure@gmail.com>
2023-12-13 01:56:08 -0800

    Command that creates table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Script that creates a table ,first_table in the current databas
+-- Database to be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS first_table
+       (`id` int, `name` varchar(256))
