acb038c8ab18c9ddff1b954b7b64597640695851
Eliud254 <eliudobure@gmail.com>
2023-12-13 04:00:47 -0800

    Command that lists the all records of the tables

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Script that list all records of the table ,second_table of the database
+-- The result should display both score and the name
+-- Record should be ordered by score
+-- The database name will be passed as an argument of the mysql command
+SELECT score, name FROM second_table ORDER BY score DESC;
