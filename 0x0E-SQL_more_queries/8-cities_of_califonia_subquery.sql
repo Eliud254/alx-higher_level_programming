06370bcb5ed352637b1d1448f23c8db7a61d7b5b
Eliud254 <eliudobure@gmail.com>
2023-12-14 12:14:54 -0800

    Correction

--- 0x0E-SQL_more_queries/8-cities_of_califonia_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_califonia_subquery.sql
@@ -1,12 +1,3 @@
--- Script that list all the cities of califonia that can be found in the database
--- The state table contains only one record where name =califonia
--- Results must be sorted in asending order by cities
--- You are noy allowed to use the join keyword
--- The database name will be passed as an argument of the mysql command
-SELECT id, name
-FROM cities
-WHERE state_id = (
-    SELECT id
-    FROM states
-    WHERE name = 'California'
-);
+-- list all the cities of California that can be found in the database hbtn_0d_usa.
+-- list all rows of a column in a database.
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
