e7284f0122153f71bad1ac7cd6f28f71fee3e090
Eliud254 <eliudobure@gmail.com>
2023-12-14 11:07:01 -0800

    List all

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- hbtn_0d_tvshows database,listing all genres not linked to the show Dexter
+-- database list all rows not linked to one row
+SELECT name
+FROM tv_genres
+WHERE name NOT IN
+(SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC;
