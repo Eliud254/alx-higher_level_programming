6fa39edd60d3680b4f67d7e4c7146fecd82e1086
Eliud254 <eliudobure@gmail.com>
2023-12-14 08:16:54 -0800

    Create table

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Script that creates the table
+-- The database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+
+
