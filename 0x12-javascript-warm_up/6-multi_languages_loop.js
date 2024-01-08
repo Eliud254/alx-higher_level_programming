e56421523aa896ea98c327420f9d15e0d3c3c615
Eliud254 <eliudobure@gmail.com>
2024-01-08 12:54:08 -0800

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool ', 'Javascript is amazing']) {
-  console.log(line);
+const langs = ["C is fun", "Python is cool", "Javascript is amazing"];
+for (let i = 0; i < 3; i++) {
+  console.log(langs[i]);
 }
