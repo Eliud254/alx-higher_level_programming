c5b0f49f83c7777d338a8ff3f23c23182352f414
Eliud254 <eliudobure@gmail.com>
2024-01-08 10:41:43 -0800

    JavaScript project

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 //  Prints a message depending of the number of arguments
 
-if (process.argv.length === 2) {
-  console.log("No argument");
-} else if (process.argv.length === 3) {
-  console.log("Argument found");
+const argsLength = process.argv.length
+
+if (argsLength === 2) {
+  console.log('No argument')
+} else if (argsLength === 3) {
+  console.log('Argument found')
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found')
 }
