b9ac48dd5a9899677f7f4dcdba6a242d693ff015
Eliud254 <eliudobure@gmail.com>
2024-01-14 09:45:01 -0800

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,9 +3,15 @@
 of a State and an instance Base = declarative_base()
 """
 import sys
-from model_state import Base, State
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import (create_engine)
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
