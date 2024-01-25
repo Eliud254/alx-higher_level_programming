3ccddeafcc146fa05838aea33b05b79448334835
Eliud254 <eliudobure@gmail.com>
2024-01-25 12:27:40 -0800

    Catch me if you can!

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# The script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -s -L -X PUT -d user_id=98 -H "Origin: School" 0.0.0.0:5000/catch_me
