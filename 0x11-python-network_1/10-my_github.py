458c6a1582332c7ea60d5cf590b7dd65986bda45
Eliud254 <eliudobure@gmail.com>
2024-01-26 10:24:45 -0800

    Done

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Acess to GitHub API and uses the information
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+
+def main(argv):
+    """
+    Takes your GitHub credentials (username and password) and
+    uses the GitHub API to display your id.
+    """
+    user = argv[1]
+    password = argv[2]
+    response = requests.get('https://api.github.com/user',
+                            auth=HTTPBasicAuth(user, password))
+    try:
+        profile_info = response.json()
+        print(profile_info['id'])
+    except KeyError:
+        print('None')
+
+
+if __name__ == "__main__":
+    main(argv)
