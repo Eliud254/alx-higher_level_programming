326280018a7b9f4bfce486dc9c7848810e99d930
Eliud254 <eliudobure@gmail.com>
2024-02-20 13:35:53 -0800

    Javasript web sraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const file = process.argv[2];
-const content = process.argv[3];
-fs.writeFile(file, content, 'utf-8', function (err) {
-  if (err) {
-    console.log(err);
-  }
-});
