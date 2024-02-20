b158f18aae83cb325cc7118d59da3a55d34076da
Eliud254 <eliudobure@gmail.com>
2024-02-20 13:00:39 -0800

    correction

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,10 @@
-#!/usr/bin/Node
+#!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8',function (error, content) {
-
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
