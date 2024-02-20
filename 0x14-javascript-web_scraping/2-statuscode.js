da3b9cf7b02fb63113ad5066fc78c3ed42f6f21a
Eliud254 <eliudobure@gmail.com>
2024-02-20 13:33:14 -0800

    Javascript web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
+const file = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
